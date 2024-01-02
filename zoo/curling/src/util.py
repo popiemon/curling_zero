f6a555c22c90dd91fb09d1a16ead1b89cef09285
popiemon <kryo.over@gmail.com>
2024-01-02 04:50:41 +0000

    util 完

--- zoo/curling/src/util.py
+++ zoo/curling/src/util.py
@@ -7,3 +7,13 @@ def calc_distance_and_neighbor_point(a, b, p):
     ba = a - b
     bp = p - b
     if np.dot(ap, ab) < 0:
+        distance = norm(ap)
+        neighbor_point = a
+    elif np.dot(bp, ba) < 0:
+        distance = norm(p -b)
+        neighbor_point = b
+    else:
+        ai_norm = np.dot(ap, ab) / norm(ab)
+        neighbor_point = a+ (ab) / norm(ab) * ai_norm
+        distance = norm(p - neighbor_point)
+    return (neighbor_point, distance)
