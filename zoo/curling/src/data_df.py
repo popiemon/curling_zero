ef1f3b015b94933c7ef9f581029c7f87e9a95187
popiemon <kryo.over@gmail.com>
2024-01-02 08:09:37 +0000

    indent 修正

--- zoo/curling/src/data_df.py
+++ zoo/curling/src/data_df.py
@@ -29,28 +29,28 @@ def stones(update: dict) -> pd.DataFrame:
         if info_flag == 0: # 1つもstoneの情報がなかったとき
             datalist.append([end, team, shot, np.nan, np.nan, np.nan])
 
-        df = pd.DataFrame(datalist, columns=['end', 'team', 'shot', 'angle', 'x', 'y'])
-        return df
+    df = pd.DataFrame(datalist, columns=['end', 'team', 'shot', 'angle', 'x', 'y'])
+    return df
     
-    def stones_fast(update: dict) -> pd.DataFrame:
-        """盤面情報のdictから stones の datafmrae を作成
+def stones_fast(update: dict) -> pd.DataFrame:
+    """盤面情報のdictから stones の datafmrae を作成
 
-        Args:
-            update (dict): 盤面情報の state dict
+    Args:
+        update (dict): 盤面情報の state dict
 
-        Returns:
-            pd.DataFrame: stones の angle, x, y が記述された dataframe
-        """
-        array = np.zeros([20, 3])
-        stone_dict = update["stones"]
-        start = 0
-        for i, key in enumerate(stone_dict.keys()):
-            for info in stone_dict[key]:
-                if info is not None:
-                    x = info["position"]["x"]
-                    y = info["position"]["y"]
-                    array[start] = np.array([i, x, y])
-                    start += 1
-        array = array[:start]
+    Returns:
+        pd.DataFrame: stones の angle, x, y が記述された dataframe
+    """
+    array = np.zeros([20, 3])
+    stone_dict = update["stones"]
+    start = 0
+    for i, key in enumerate(stone_dict.keys()):
+        for info in stone_dict[key]:
+            if info is not None:
+                x = info["position"]["x"]
+                y = info["position"]["y"]
+                array[start] = np.array([i, x, y])
+                start += 1
+    array = array[:start]
 
-        return array
+    return array
